075380dc79758bc5be7fe4d3bb05ad0381181028
Luke Bakken <luke@bakken.io>
2025-01-09 08:36:17 -0800

    tweaks

--- repro.py
+++ repro.py
@@ -1,27 +1,20 @@
 import logging
 import pika
-import time
 
 LOG_FORMAT = "%(levelname) -10s %(asctime)s %(name) -30s %(funcName) -35s %(lineno) -5d: %(message)s"
 LOGGER = logging.getLogger(__name__)
 
 logging.basicConfig(level=logging.ERROR, format=LOG_FORMAT)
 
-while True:
-    try:
-        c = pika.BlockingConnection()
-        ch = c.channel()
+c = pika.BlockingConnection()
+ch = c.channel()
 
-        # Note: exclusive=False results in unbounded queue count
-        # and eventual memory alarm
-        q = ch.queue_declare("", exclusive=True)
-        print("q: {}".format(q), flush=True)
+LOGGER.info("declaring exclusive queue")
 
-        ch.queue_bind(queue="", exchange="amq.direct", routing_key="foobar")
-        ch.close()
-        c.close()
-    except pika.exceptions.ChannelClosedByBroker:
-        print("BOOM")
-        c.close()
+# Note: exclusive=False results in unbounded queue count
+# and eventual memory alarm
+q = ch.queue_declare("", exclusive=True)
+print("q: {}".format(q), flush=True)
 
-    time.sleep(2)
+LOGGER.info("declared exclusive queue, sleeping 30 seconds")
+c.process_data_events(time_limit=30)
